9a0ca81f3e3bce724ef09360f2a445de32a5c8f2
Radhika-Deshmukh <97799763+Radhika-Deshmukh@users.noreply.github.com>
2025-10-30 03:34:03 +0530

    tictac

--- tictac.py
+++ tictac.py
@@ -1,100 +1,126 @@
-import re, random
-from colorama import Fore, init
-
-# Initialize colorama (autoreset ensures each print resets after use)
+import random
+from colorama import init, Fore, Style
 init(autoreset=True)
 
-# Destination & joke data
-destinations = {
-    "beaches": ["Bali", "Maldives", "Phuket"],
-    "mountains": ["Swiss Alps", "Rocky Mountains", "Himalayas"],
-    "cities": ["Tokyo", "Paris", "New York"]
-}
 
-jokes = [
-    "Why don't programmers like nature? Too many bugs!",
-    "Why did the computer go to the doctor? Because it had a virus!",
-    "Why do travelers always feel warm? Because of all their hot spots!"
-]
-
-# Helper function to normalize user input (remove extra spaces, make lowercase)
-def normalize_input(text):
-    return re.sub(r"\s+", " ", text.strip().lower())
-
-# Provide travel recommendations (recursive if user rejects suggestions)
-def recommend():
-    print(Fore.CYAN + "TravelBot: Beaches, mountains, or cities?")
-    preference = input(Fore.YELLOW + "You: ")
-    preference = normalize_input(preference)
-
-    if preference in destinations:
-        suggestion = random.choice(destinations[preference])
-        print(Fore.GREEN + f"TravelBot: How about {suggestion}?")
-        print(Fore.CYAN + "TravelBot: Do you like it? (yes/no)")
-        answer = input(Fore.YELLOW + "You: ").lower()
-
-        if answer == "yes":
-            print(Fore.GREEN + f"TravelBot: Awesome! Enjoy {suggestion}!")
-        elif answer == "no":
-            print(Fore.RED + "TravelBot: Let's try another.")
-            recommend()
+def display_board(board):
+    print()
+    def colored(cell):
+        if cell == 'X':
+            return Fore.RED + cell + Style.RESET_ALL
+        elif cell == 'O':
+            return Fore.BLUE + cell + Style.RESET_ALL
         else:
-            print(Fore.RED + "TravelBot: I'll suggest again.")
-            recommend()
+            return Fore.YELLOW + cell + Style.RESET_ALL
+    print(' ' + colored(board[0]) + ' | ' + colored(board[1]) + ' | ' + colored(board[2]))
+    print(Fore.CYAN + '-----------' + Style.RESET_ALL)
+    print(' ' + colored(board[3]) + ' | ' + colored(board[4]) + ' | ' + colored(board[5]))
+    print(Fore.CYAN + '-----------' + Style.RESET_ALL)
+    print(' ' + colored(board[6]) + ' | ' + colored(board[7]) + ' | ' + colored(board[8]))
+    print()
+
+
+def player_choice():
+    symbol = ''
+    while symbol not in ['X', 'O']:
+        symbol = input(Fore.GREEN + "Do you want to be X or O? " + Style.RESET_ALL).upper()
+    if symbol == 'X':
+        return ('X', 'O')
     else:
-        print(Fore.RED + "TravelBot: Sorry, I don't have that type of destination.")
-        show_help()
+        return ('O', 'X')
 
-# Offer packing tips based on user’s destination and duration
-def packing_tips():
-    print(Fore.CYAN + "TravelBot: Where to?")
-    location = normalize_input(input(Fore.YELLOW + "You: "))
-    print(Fore.CYAN + "TravelBot: How many days?")
-    days = input(Fore.YELLOW + "You: ")
 
-    print(Fore.GREEN + f"TravelBot: Packing tips for {days} days in {location}:")
-    print(Fore.GREEN + "- Pack versatile clothes.")
-    print(Fore.GREEN + "- Bring chargers/adapters.")
-    print(Fore.GREEN + "- Check the weather forecast.")
+def player_move(board, symbol):
+    move = -1
+    while move not in range(1, 10) or not board[move - 1].isdigit():
+        try:
+            move = int(input("Enter your move (1-9): "))
+            if move not in range(1, 10) or not board[move - 1].isdigit():
+                print("Invalid move. Please try again.")
+        except ValueError:
+            print("Please enter a number between 1 and 9.")
+    board[move - 1] = symbol
 
-# Tell a random joke
-def tell_joke():
-    print(Fore.YELLOW + f"TravelBot: {random.choice(jokes)}")
 
-# Display help menu
-def show_help():
-    print(Fore.MAGENTA + "\nI can:")
-    print(Fore.GREEN + "- Suggest travel spots (say 'recommendation')")
-    print(Fore.GREEN + "- Offer packing tips (say 'packing')")
-    print(Fore.GREEN + "- Tell a joke (say 'joke')")
-    print(Fore.GREEN + "- Type 'exit' or 'bye' to end.\n")
+def ai_move(board, ai_symbol, player_symbol):
+    for i in range(9):
+        if board[i].isdigit():
+            board_copy = board.copy()
+            board_copy[i] = ai_symbol
+            if check_win(board_copy, ai_symbol):
+                board[i] = ai_symbol
+                return
+    for i in range(9):
+        if board[i].isdigit():
+            board_copy = board.copy()
+            board_copy[i] = player_symbol
+            if check_win(board_copy, player_symbol):
+                board[i] = ai_symbol
+                return
+    possible_moves = [i for i in range(9) if board[i].isdigit()]
+    move = random.choice(possible_moves)
+    board[move] = ai_symbol
 
-# Main chat loop
-def chat():
-    print(Fore.CYAN + "Hello! I'm TravelBot.")
-    name = input(Fore.YELLOW + "Your name? ")
-    print(Fore.GREEN + f"Nice to meet you, {name}!")
 
-    show_help()
+def check_win(board, symbol):
+    win_conditions = [
+        (0, 1, 2), (3, 4, 5), (6, 7, 8),  # Horizontal
+        (0, 3, 6), (1, 4, 7), (2, 5, 8),  # Vertical
+        (0, 4, 8), (2, 4, 6)              # Diagonal
+    ]
+    for cond in win_conditions:
+        if board[cond[0]] == board[cond[1]] == board[cond[2]] == symbol:
+            return True
+    return False
 
+
+def check_full(board):
+    return all(not spot.isdigit() for spot in board)
+
+
+def tic_tac_toe():
+    print("Welcome to Tic-Tac-Toe!")
+    player_name = input(Fore.GREEN + "Enter your name: " + Style.RESET_ALL)
     while True:
-        user_input = input(Fore.YELLOW + f"{name}: ")
-        user_input = normalize_input(user_input)
+        board = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+        player_symbol, ai_symbol = player_choice()
+        turn = 'Player'
+        game_on = True
 
-        if "recommend" in user_input or "suggest" in user_input:
-            recommend()
-        elif "pack" in user_input or "packing" in user_input:
-            packing_tips()
-        elif "joke" in user_input or "funny" in user_input:
-            tell_joke()
-        elif "help" in user_input:
-            show_help()
-        elif "exit" in user_input or "bye" in user_input:
-            print(Fore.CYAN + "TravelBot: Safe travels! Goodbye!")
+        while game_on:
+            display_board(board)
+            if turn == 'Player':
+                player_move(board, player_symbol)
+                if check_win(board, player_symbol):
+                    display_board(board)
+                    print("Congratulations! " + player_name + ", you have won the game!")
+                    game_on = False
+                else:
+                    if check_full(board):
+                        display_board(board)
+                        print("It's a tie!")
+                        break
+                    else:
+                        turn = 'AI'
+            else:
+                ai_move(board, ai_symbol, player_symbol)
+                if check_win(board, ai_symbol):
+                    display_board(board)
+                    print("AI has won the game!")
+                    game_on = False
+                else:
+                    if check_full(board):
+                        display_board(board)
+                        print("It's a tie!")
+                        break
+                    else:
+                        turn = 'Player'
+
+        play_again = input("Do you want to play again? (yes/no): ").lower()
+        if play_again != 'yes':
+            print("Thank you for playing!")
             break
-        else:
-            print(Fore.RED + "TravelBot: Could you rephrase?")
 
-# Run the chatbot
+
 if __name__ == "__main__":
-    chat()
+    tic_tac_toe()
